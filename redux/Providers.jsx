d86c8defecfa6ba71305d64da023ef45772130c7
Harsh Lad <harshplad@gmail.com>
2024-04-07 14:13:24 +0530

    added analytics

--- redux/Providers.jsx
+++ redux/Providers.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import store from './store'
 import { ToastContainer } from 'react-toastify'
-
+import { Analytics } from "@vercel/analytics/react"
 function Providers({ children }) {
     return (
         <Provider store={store}>
@@ -19,6 +19,7 @@ function Providers({ children }) {
                 pauseOnHover
                 theme="light"
             />
+            <Analytics/>
             <div>{children}</div>
         </Provider>
     )
